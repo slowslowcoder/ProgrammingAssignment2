dfbc4d787b6a95bf410ce41a5e742e2448583009
slowslowcoder <mr_t_dtone@hotmail.com>
2014-05-20 23:29:47 +0900

    update cachematrix*

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The matrix inversion is a time-consuming operation.
+## This file provide a function that can be used when
+## the inversion of the same matrix is repeatedly called.
+## Instead of repeatedly calculating that matrix conversion,
+## the result of the last call is cached, and the function
+## will return the cached result if the same matrix inversion
+## is called.
 
-## Write a short comment describing this function
+## Make Cache Matrix will return a list of functions, 
+## set, get, setsolve, getsolve.
+## "set" will set the input, and "get" is the function to get it.
+## "setsolve" will calculate the inversion, 
+## and "getsolve" will return the calculation result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The argument of cacheSolve is the list of function
+## (set, get, setsolve, getsolve), which has the same
+## meaning as the output of makeCacheMatrix
+
+## It will calculate matrix inversion if required, and
+## return the cached result if available.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
